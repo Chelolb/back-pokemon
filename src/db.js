de0e5d54ec4c91f9526322436aab07b192b98279
Chelolb <marcelolitwin@gmail.com>
2022-10-22 11:54:17 -0300

    update for render

--- src/db.js
+++ src/db.js
@@ -3,12 +3,13 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  DB_URI
+  } = process.env;
+  
+  //  Conexion con DB PostgreSQL alojada en servidor externo
+  const sequelize = new Sequelize(`${DB_URI}`, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 
